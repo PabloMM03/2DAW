01a4a42f50f98082617b03842d17111c0c389726
PabloMM03 <1V3rsus1@gmail.com>
2023-02-01 19:46:56 +0100

    s

--- DWEC_PMM/2ºTrimestre/Unidad4/Boletin1/Restriciones Complejas/js/script.js
+++ DWEC_PMM/2ºTrimestre/Unidad4/Boletin1/Restriciones Complejas/js/script.js
@@ -1,110 +1,128 @@
 const CLASE_ERROR_CAMPO = "error";
 const CLASE_ERROR_MENSAJES = "campoError";
 
-const MIN_INTERESES = 2; 
+const MIN_INTERESES = 2;
 
 crearListeners();
 
-function crearListeners(){
+function crearListeners() {
 
-    document.getElementById("formulario").setAttribute("novalidate", true);
+    //Añadir el novalidate a el formulario
+    document.getElementById("formulario").setAttribute("novalidate",true);
 
-    document.getElementById("nombre").addEventListener("blur", validarNombreEvento, false);
+    document.getElementById("pass").addEventListener("blur", validarPassEvento, false);
+    document.getElementById("confirmPass").addEventListener("blur", validarPassEvento, false);
 
-    document.getElementById("nombre").addEventListener("invalid", notificarErrorNombreEvento, false);
+    document.getElementById("pass").addEventListener("invalid", notificarErrorPassEvento, false);
+    document.getElementById("confirmPass").addEventListener("invalid", notificarErrorPassEvento, false);
 
-    document.getElementById("nombre").addEventListener("input", revisarErroresEvento, false);
+    document.getElementById("pass").addEventListener("input", revisarErroresEvento, false);
+    document.getElementById("confirmPass").addEventListener("input", revisarErroresEvento, false);
+
+    
+
 }
 
-function validarNombreEvento(e){
-    const nombre = e.target;
-    actualizarErroresNombre(nombre);
-    validarCampo(nombre);
+function validarPassEvento(e) {
+    const pass = e.target;
+    const confirmPass = e.target;
+    actualizarErroresPass(pass,confirmPass);
+
+    validarCampo(pass);
 }
 
-function actualizarErroresNombre(nombre){
-
-    const contenido = nombre.value;
-
+function actualizarErroresPass(pass) {
+    const contenido = pass.value;
+    const contenido2 = confirmPass.value;
     let mensaje = "";
 
-    if(contenido === ""){
-        mensaje = `El campo ${nombre.name} no puede estar vacio`;
-    }else if((contenido.length < 8) && (contenido.length > 0)){
-        mensaje = `El campo ${nombre.name} tiene que tener minimo 8 caracteres`;
-    }else if(contenido.length > 20){
-        mensaje = `El campo ${nombre.name} debe de tener un maximo de 20 caracteres`;
+    if(contenido === "") {
+        mensaje = `El campo ${pass.name} no puede estar vacío`;
+    }if(contenido2 === ""){
+        mensaje = `El campo ${confirmPass.name} no puede estar vacío`;
+    }if(contenido !== contenido2) {
+        mensaje = `El campo ${pass.name} no puede ser diferente a confirmPass`;
     }
-    nombre.setCustomValidity(mensaje);
+    pass.setCustomValidity(mensaje);
+    
 }
 
-function validarNombreEvento(e){
+
+function validarCampoEvento(e) {
     return validarCampo(e.target);
 }
 
-function validarCampo(campo){
+function validarCampo(campo) {
     eliminarErrores(campo);
     return campo.checkValidity();
 }
 
-function revisarErroresEvento(e){
+function revisarErroresEvento(e) {
     const campo = e.target;
-
     actualizarErroresNombre(campo);
-    
-    if(campo.validity.valid){
+    if(campo.validity.valid) {
         eliminarErrores(campo);
     }
 }
 
-function notificarErrorNombreEvento(e){
-    const nombre = e.target;
+function notificarErrorPassEvento(e) {
+    const pass = e.target;
+    
     let mensajes = [];
-
-    if(nombre.validity.cutomError){
-        mensajes.push(nombre.validationMessage);
+    if(pass.validity.customError) {
+        mensajes.push(pass.validationMessage);
     }
-    mostrarMensajesErrorEn(nombre,mensajes);
+    
+    mostrarMensajesErrorEn(mensajes, pass);
 }
 
-function hayErrorEnCampo(campo){
+function hayErrorEnCampo(campo) {
     return campo.classList.contains(CLASE_ERROR_CAMPO);
 }
 
-function eliminarErrores(campo){
- campo.classList.remove(CLASE_ERROR_CAMPO);
-
- const contenedorErrores = document.getElementById(`${campo.name}-${CLASE_ERROR_CAMPO}`);
- if(contenedorErrores){
-    contenedorErrores.parentElement.removeChild(contenedorErrores);
- }
-
+function eliminarErrores(campo) {
+    // Eliminamos la clase de error del elemento
+    campo.classList.remove(CLASE_ERROR_CAMPO);
+    // Eliminamos los mensajes de error
+    const contenedorErrores = document.getElementById(`${campo.name}-${CLASE_ERROR_CAMPO}`);
+    if(contenedorErrores) {
+        contenedorErrores.parentElement.removeChild(contenedorErrores);
+    }
 }
 
-function mostrarMensajesErrorEn(campo,mensajes){
-    campo.classList.add(CLASE_ERROR_MENSAJES);
-
+function mostrarMensajesErrorEn(mensajes, campo) {
+    // añadimos la clase que cambia el borde del campo de negro a rojo
+    campo.classList.add(CLASE_ERROR_CAMPO);
     let contenedorMensajes = document.createElement("div");
+    // Añadimos la clase para el texto
+    contenedorMensajes.classList.add(CLASE_ERROR_MENSAJES);
 
+    contenedorMensajes.setAttribute("aria-describedby",campo.id);
+    // Añadimos el id del div para después poder eliminarlo con facilidad
     contenedorMensajes.id = `${campo.name}-${CLASE_ERROR_CAMPO}`;
-
-    for(let mensaje of mensajes){
+    for(let mensaje of mensajes) {
         let parrafo = document.createElement("p");
         parrafo.textContent = mensaje;
-
+        // Añadimos el párrafo al contenedor de mensajes
         contenedorMensajes.appendChild(parrafo);
-
     }
     insertarDespues(campo, contenedorMensajes);
 }
 
+/**
+ * Método de utilidad que añade la funcionalidad al DOM de añadir un elemento (campoAnadir) HTML detrás
+ * de otro elemento del DOM indicado (campoReferencia)
+ * @param {Element} campoReferencia campo detrás del que hay que añadir el nuevo elemento
+ * @param {Element} campoAnadir nuevo elemento que hay que añadir
+ */
 function insertarDespues(campoReferencia, campoAnadir){
-
     if(campoReferencia.nextSibling){
-        campoReferencia.parentNode.insertBefore(campoReferencia.nextSibling);
-    }else{
-        campoReferencia.parentNode.appendChild(campoAnadir);
+        // El elemento de referencia tiene un hermano detrás
+        // El elemento a añadir se añade después del siguiente hermano de "campoReferencia"
+        campoReferencia.parentNode.insertBefore(campoAnadir,campoReferencia.nextSibling); 
+    } else { 
+        // El elemento de referencia no tiene un hermano detrás
+        // Se añade como último hijo de su padre
+        campoReferencia.parentNode.appendChild(campoAnadir); 
     }
-
-
 }
