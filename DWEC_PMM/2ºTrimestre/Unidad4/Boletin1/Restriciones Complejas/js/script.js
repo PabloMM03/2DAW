f45ba5f4f4ecd10e7af828428b2a53f0fa2d01d1
PabloMM03 <1V3rsus1@gmail.com>
2023-02-04 17:39:12 +0100

    Terminado

--- DWEC_PMM/2ºTrimestre/Unidad4/Boletin1/Restriciones Complejas/js/script.js
+++ DWEC_PMM/2ºTrimestre/Unidad4/Boletin1/Restriciones Complejas/js/script.js
@@ -2,7 +2,8 @@ const CLASE_ERROR_CAMPO = "error";
 const CLASE_ERROR_MENSAJES = "campoError";
 
 const MIN_INTERESES = 2;
-
+const MIN_EDAD = 18;
+const MAX_EDAD = 100;
 crearListeners();
 
 function crearListeners() {
@@ -10,92 +11,142 @@ function crearListeners() {
     //Añadir el novalidate a el formulario
     document.getElementById("formulario").setAttribute("novalidate",true);
 
+    document.getElementById("nombre").addEventListener("blur", validarNombreEvento, false);
     document.getElementById("pass").addEventListener("blur", validarPassEvento, false);
-    document.getElementById("confirmPass").addEventListener("blur", validarPass2Evento, false);
-    // document.getElementById("genero").addEventListener("blur", validarHombreEvento, false);
-    // document.getElementById("mujer").addEventListener("blur", validarMujerEvento, false);
+    document.getElementById("pass2").addEventListener("blur", validarPass2Evento, false);
+    document.getElementById("edad").addEventListener("blur", validarEdadEvento, false);
+    document.getElementById("genero").addEventListener("blur", validarGeneroEvento, false);
+    document.getElementById("vehicle1").addEventListener("blur", validarInt1Evento, false);
+    document.getElementById("vehicle2").addEventListener("blur", validarInt1Evento, false);
 
+
+    document.getElementById("nombre").addEventListener("invalid", notificarErrorNombreEvento, false);
     document.getElementById("pass").addEventListener("invalid", notificarErrorPassEvento, false);
-    document.getElementById("confirmPass").addEventListener("invalid", notificarErrorPass2Evento, false);
-    // document.getElementById("genero").addEventListener("invalid", notificarErrorHombreEvento, false);
-    // document.getElementById("mujer").addEventListener("invalid", notificarErrorMujerEvento, false);
+    document.getElementById("pass2").addEventListener("invalid", notificarErrorPass2Evento, false);
+    document.getElementById("edad").addEventListener("invalid", notificarErrorEdadEvento, false);
+    document.getElementById("genero").addEventListener("invalid", notificarErrorGeneroEvento, false);
+    document.getElementById("vehicle1").addEventListener("invalid", notificarErrorInt1Evento, false);
+    document.getElementById("vehicle2").addEventListener("invalid", notificarErrorInt1Evento, false);
 
-    document.getElementById("pass").addEventListener("input", revisarErroresEvento, false);
-    document.getElementById("confirmPass").addEventListener("input", revisarErroresEvento, false);
-    // let genero = document.querySelector('input[name="genero"]:checked');
-    // document.getElementById("genero").addEventListener("input", revisarErroresEvento, false);
-    // document.getElementById("mujer").addEventListener("input", revisarErroresEvento, false);
-
+    document.getElementById("nombre").addEventListener("input", revisarErroresNombreEvento, false);
+    document.getElementById("pass").addEventListener("input", revisarErroresPassEvento, false);
+    document.getElementById("pass2").addEventListener("input", revisarErroresPass2Evento, false);
+    document.getElementById("edad").addEventListener("input", revisarErroresEdadEvento, false);
+    document.getElementById("genero").addEventListener("input", revisarErroresGeneroEvento, false);
+    document.getElementById("vehicle1").addEventListener("input", revisarErroresInt1Evento, false);
+    document.getElementById("vehicle2").addEventListener("input", revisarErroresInt1Evento, false);
     
+    document.getElementById("formulario").addEventListener("submit", validarFormularioEvento, false);
+
 
 }
-// if(!genero){
-//     alert("Debe seleccionar un genero");
-// }
-//PASS
 
-function validarPassEvento(e) {
+//Nombre
+function validarNombreEvento(e) {
+    const nombre = e.target;
+    actualizarErroresNombre(nombre);
+    validarCampo(nombre);
+}
+function actualizarErroresNombre(nombre) {
+    const contenido = nombre.value;
+    let mensaje = "";
+    if(contenido === "") {
+        mensaje = `El campo ${nombre.name} no puede estar vacío`;
+    }else if((contenido.length < 8)&&(contenido.length > 0)) {
+        mensaje = `El campo ${nombre.name} debe tener al menos 8 caracteres`;
+    }else if(contenido.length > 20) {
+        mensaje = `El campo ${nombre.name} debe tener un máximo de 20 caracteres`;
+    }
+    nombre.setCustomValidity(mensaje);
+}
+//Edad
+function validarEdadEvento(e){
+    const edad = e.target;
+    actualizarErroresEdad(edad);
+    validarCampo(edad);
+}
+
+function actualizarErroresEdad(edad){
+    const contenido = edad.value;
+    let mensaje = "";
+
+    if(contenido === ""){
+        mensaje = `El campo ${edad.name} no puede estar vacío`;
+    }else if((contenido < MIN_EDAD)|| (contenido > MAX_EDAD)){
+        mensaje = `El campo ${edad.name} tiene que tener un valor entre ${MIN_EDAD} y ${MAX_EDAD}`;
+    }
+    edad.setCustomValidity(mensaje);
+}
+//Pass
+function validarPassEvento(e){
     const pass = e.target;
-    actualizarErroresPass(pass,confirmPass);
-
+    const pass2 = e.target;
+    actualizarErroresPass(pass);
     validarCampo(pass);
 }
-//CONFIRM PASS
-function validarPass2Evento(e) {
-   
-    const confirmPass = e.target;
-    actualizarErroresPass2(confirmPass,pass);
-
-    validarCampo(confirmPass);
-}
-//GENERO
-//  function validarHombreEvento(e) {
-   
-//      const genero = e.target;
-//      actualizarErroresHombre(genero);
-
-//     validarCampo(genero);
-//  }
-//PASS
-function actualizarErroresPass(pass,confirmPass) {
+function actualizarErroresPass(pass){
     const contenido = pass.value;
-    const contenido2 = confirmPass.value;
+    const contenido2 = pass2.value
     let mensaje = "";
 
-    if(contenido === "") {
-        mensaje = `El campo ${pass.name} no puede estar vacío`;
-    }if(contenido !== contenido2) {
-        mensaje = `El campo ${pass.name} no puede ser diferente a confirmPass`;
+    if(contenido === ""){
+        mensaje = `EL campo ${pass.name} no puede estar vacío`;
+    }if(contenido !== contenido2){
+        mensaje = `El campo ${pass.name} no puede ser diferente a ${pass2.name}`;       
     }
     pass.setCustomValidity(mensaje);
-    
 }
-//CONFIRM PASS
-function actualizarErroresPass2(confirmPass,pass) {
-    const contenido2 = confirmPass.value;
-    const contenido = pass.value;
+//Pass2
+function validarPass2Evento(e){
+    const pass = e.target;
+    const pass2 = e.target;
+    actualizarErroresPass2(pass2);
+    validarCampo(pass2);
+}
+function actualizarErroresPass2(pass2){
+    const contenido = pass2.value;
+    const contenido2 = pass.value;
     let mensaje = "";
 
-    if(contenido2 === ""){
-        mensaje = `El campo confirmPass no puede estar vacío`;
-    }if(contenido2 !== contenido) {
-        mensaje = `El campo confirmPass no puede ser diferente a ${pass.name}`;
+    if(contenido === ""){
+        mensaje = `EL campo ${pass2.name} no puede estar vacío`;
+    }if((contenido !== contenido2)){
+        mensaje = `El campo ${pass2.name} no puede ser diferente a ${pass.name}`;  
     }
-    confirmPass.setCustomValidity(mensaje);
-    
+    pass2.setCustomValidity(mensaje);
 }
-//GENERO
-//   function actualizarErroresHombre(genero) {
-//       const genero = genero.value;
+//Genero
+ function validarGeneroEvento (e){
+     const genero = e.target;
+    actualizarErroresGenero(genero);
+     validarCampo(genero);
+ }
+ function actualizarErroresGenero(genero){
+     const contenido = document.querySelector('input[name="genero"]:checked');
+     let mensaje = "";
 
-//       let mensaje = "";
+     if(!contenido) {
+       mensaje = `Debe seleccionar un genero`;
+       }
+     genero.setCustomValidity(mensaje);
+ }
+ //Interes1
+ function validarInt1Evento (e){
+    const vehicle1 = e.target;
+    const vehicle2 = e.target;
+   actualizarErroresInt1(vehicle1);
+    validarCampo(vehicle1);
+}
+function actualizarErroresInt1(vehicle1){
+    const contenido = vehicle1.checked;
+    const contenido2 = vehicle2.checked;
+    let mensaje = "";
 
-//       if(genero){
-//         mensaje = `Debe seleccionar un genero`;
-//       }
-//       genero.setCustomValidity(mensaje);
-    
-//  }
+    if(!contenido && !contenido2) {
+      mensaje = `Debe seleccionar almenos dos intereses`;
+      }
+    vehicle1.setCustomValidity(mensaje);
+}
 
 function validarCampoEvento(e) {
     return validarCampo(e.target);
@@ -105,47 +156,106 @@ function validarCampo(campo) {
     eliminarErrores(campo);
     return campo.checkValidity();
 }
-
-function revisarErroresEvento(e) {
+//Nombre
+function revisarErroresNombreEvento(e) {
     const campo = e.target;
-    actualizarErroresPass(campo);
+    actualizarErroresNombre(campo);
     if(campo.validity.valid) {
         eliminarErrores(campo);
     }
 }
-//PASS
-function notificarErrorPassEvento(e) {
-    const pass = e.target;
-    
+
+function notificarErrorNombreEvento(e) {
+    const nombre = e.target;
     let mensajes = [];
-    if(pass.validity.customError) {
+    if(nombre.validity.customError) {
+        mensajes.push(nombre.validationMessage);
+    }
+    mostrarMensajesErrorEn(mensajes, nombre);
+}
+//Edad
+function revisarErroresEdadEvento(e) {
+    const campo = e.target;
+    actualizarErroresEdad(campo);
+    if(campo.validity.valid) {
+        eliminarErrores(campo);
+    }
+}
+function notificarErrorEdadEvento(e) {
+    const edad = e.target;
+    let mensajes = [];
+    if(edad.validity.customError) {
+        mensajes.push(edad.validationMessage);
+    }
+    mostrarMensajesErrorEn(mensajes, edad);
+}
+//Pass
+function revisarErroresPassEvento(e){
+    const campo = e.target;
+    actualizarErroresPass(campo);
+    if(campo.validity.valid){
+        eliminarErrores(campo);
+    }
+}
+function notificarErrorPassEvento(e){
+    const pass = e.target;
+    let mensajes = [];
+    if(pass.validity.customError){
         mensajes.push(pass.validationMessage);
     }
-    
     mostrarMensajesErrorEn(mensajes, pass);
 }
-//CONFIRM PASS
-function notificarErrorPass2Evento(e) {
-    const confirmPass = e.target;
-    
-    let mensajes = [];
-    if(confirmPass.validity.customError) {
-        mensajes.push(confirmPass.validationMessage);
+
+//Pass2
+function revisarErroresPass2Evento(e){
+    const campo = e.target;
+    actualizarErroresPass2(campo);
+    if(campo.validity.valid){
+        eliminarErrores(campo);
     }
-    
-    mostrarMensajesErrorEn(mensajes, confirmPass);
 }
-//GENERO
-//  function notificarErrorGeneroEvento(e) {
-//      const genero = e.target;
-    
-//      let mensajes = [];
-//      if(genero.validity.customError) {
-//          mensajes.push(genero.validationMessage);
-//      }
-//  }
+function notificarErrorPass2Evento(e){
+    const pass2 = e.target;
+    let mensajes = [];
+    if(pass2.validity.customError){
+        mensajes.push(pass2.validationMessage);
+    }
+    mostrarMensajesErrorEn(mensajes, pass2);
+}
+
+//Genero
+ function revisarErroresGeneroEvento(e){
+     const genero = e.target;
+     actualizarErroresGenero(campo);
+     if(genero.validity.valid){
+         eliminarErrores(genero);
+     }
+}
+ function notificarErrorGeneroEvento(e){
+     const genero = e.target;
+     let mensajes = [];
+     if(genero.validity.customError){
+         mensajes.push(genero.validationMessage);
+     }
      mostrarMensajesErrorEn(mensajes, genero);
-// }
+ }
+ //Interes1
+ function revisarErroresInt1Evento(e){
+    const vehicle1 = e.target;
+    actualizarErroresInt1(campo);
+    if(vehicle1.validity.valid){
+        eliminarErrores(vehicle1);
+    }
+}
+function notificarErrorInt1Evento(e){
+    const vehicle1 = e.target;
+    let mensajes = [];
+    if(vehicle1.validity.customError){
+        mensajes.push(vehicle1.validationMessage);
+    }
+    mostrarMensajesErrorEn(mensajes, vehicle1);
+}
+
 function hayErrorEnCampo(campo) {
     return campo.classList.contains(CLASE_ERROR_CAMPO);
 }
@@ -194,5 +304,20 @@ function insertarDespues(campoReferencia, campoAnadir){
         // El elemento de referencia no tiene un hermano detrás
         // Se añade como último hijo de su padre
         campoReferencia.parentNode.appendChild(campoAnadir); 
+    }
+}
+function validarFormularioEvento(e){
+    let formValido = validarCampo(document.getElementById("pass"));
+    formValido = validarCampo(document.getElementById("pass2"))&& formValido;
+    formValido = validarCampo(document.getElementById("nombre"))&& formValido;
+    formValido = validarCampo(document.getElementById("edad"))&& formValido;
+    formValido = validarCampo(document.getElementById("genero"))&& formValido;
+    formValido = validarCampo(document.getElementById("vehicle1"))&& formValido;
+
+    if(formValido){
+        console.log("El formulario está validado correctamente sin errores.");
+    }else{
+        e.preventDefault();
+        console.error("El formulario no está validado ya que tiene errores.");
     }
 }
