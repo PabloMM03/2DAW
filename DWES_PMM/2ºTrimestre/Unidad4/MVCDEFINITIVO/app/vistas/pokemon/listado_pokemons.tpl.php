f66f63bc123450eb3413d9253acbbde1138ebb3d
PabloMM03 <1V3rsus1@gmail.com>
2023-02-04 19:03:14 +0100

    Cosas

--- DWES_PMM/2ºTrimestre/Unidad4/MVCDEFINITIVO/app/vistas/pokemon/listado_pokemons.tpl.php
+++ DWES_PMM/2ºTrimestre/Unidad4/MVCDEFINITIVO/app/vistas/pokemon/listado_pokemons.tpl.php
@@ -95,5 +95,18 @@
                 }, 1000);
         }, false);
 
+        document.querySelector('#pag2').addEventListener('click', ()=>{
+        let xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+                        if (this.readyState == 4 && this.status == 200) {
+                                let tabla = document.getElementById("pokemonsTabla");
+                                tabla.innerHTML = xhttp.response--;
+                        }
+    });
+    setTimeout(() => {
+                        xhttp.open("GET", "./?controlador=pokemon&source=api&metodo=tandaPokemons", true);
+                        xhttp.send();
+                }, 1000);
+        }, false);
 </script>
 <?php include_once('./app/vistas/inc/footer.tpl.php'); ?>
