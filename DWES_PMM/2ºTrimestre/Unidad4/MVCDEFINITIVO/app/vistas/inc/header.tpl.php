83ac0b36d0fd0d8f7099b7ad23e1aab70fc57125
PabloMM03 <1V3rsus1@gmail.com>
2023-02-05 04:11:15 +0100

    SI

--- DWES_PMM/2ºTrimestre/Unidad4/MVCDEFINITIVO/app/vistas/inc/header.tpl.php
+++ DWES_PMM/2ºTrimestre/Unidad4/MVCDEFINITIVO/app/vistas/inc/header.tpl.php
@@ -3,6 +3,9 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" type="text/css" href="./public/css/estilos.css">
+    <link rel="stylesheet" type="text/css" href="./public/css/style.css">
+    <script src="https://kit.fontawesome.com/2d6ecfbd70.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700&display=swap" rel="stylesheet" />
     <title>Gestor de pokemos BETA</title>
 </head>
 <body>
