dd376128351c0ff04037e5c25607af86cf385cd8
PabloMM03 <1V3rsus1@gmail.com>
2023-02-12 18:39:07 +0100

    Cosas

--- DWEC_PMM/2ºTrimestre/Unidad5/ModelosDeAtributos/NodosDeTipoAtribute/scripts.js
+++ DWEC_PMM/2ºTrimestre/Unidad5/ModelosDeAtributos/NodosDeTipoAtribute/scripts.js
@@ -44,3 +44,34 @@ const parrafo =  document.querySelectorAll("p");
 parrafo.forEach(p=>{
      p.removeAttribute("title");
 });
+
+
+///////////////////////NODOS DE TIPO TEXT/////////////////////////////
+//a) Recupera el contenido del párrafo dentro del div identificado como callout2 usando:
+
+/*
+La propiedad innerHTML.
+ La propiedad textContent.
+ La propiedad nodeValue. Ten en cuenta que el primer hijo de un elemento es el texto que
+contiene pero comprueba que no haya espacios en blanco entre el elemento div y p. Puede
+tomar el primer hijo de div como un espacio en blanco. 
+*/
+
+//InnerHtml
+let callout2 = document.getElementById("callout2");
+let paraContent = callout2.innerHTML;
+console.log(paraContent);
+
+//TextContent
+paraContent = callout2.textContent;
+console.log(paraContent);
+
+//NodeValue
+let parrafoEnCuestion = callout2.getElementsByTagName("p")[0];
+let contenido = parrafoEnCuestion.firstChild.nodeValue;
+console.log(contenido);
+
+//b) Modifica el contenido del primer párrafo del div callout2 para añadir el texto “pero bien formado.”
+
+parrafoEnCuestion.innerHTML = parrafoEnCuestion.innerHTML + " pero bien formado";
+
