2847b88bbc61772fafcfa26d1666f2fe1f6ad1c5
PabloMM03 <1V3rsus1@gmail.com>
2023-02-13 20:08:22 +0100

    Tienda

---
+++ DWEC_PMM/2ºTrimestre/Unidad5/Tienda online LocalStorage/js/app.js
@@ -0,0 +1,3 @@
+// Importamos la base de datos
+import {productos} from "../db/db.js";
+
---
+++ DWEC_PMM/2ºTrimestre/Unidad5/Tienda online LocalStorage/js/controlador.js
@@ -0,0 +1,31 @@
+// Importamos el modelo de datos
+import {productos, categorias} from "../db/db.js";
+import {Util} from "./util.js"; 
+
+
+export class ControladorDB {
+   
+    static getProductos() {
+        return productos;
+    }
+    
+    static getProducto(id) {
+        const productoArray = productos.filter(producto => producto.id === id);
+        return productoArray.length === 0 ? null : productoArray[0];
+    }
+
+   
+    static getCategorias() {
+        return categorias;
+    }
+
+    static getCategoria(id) {
+        const categoriaArray = categorias.filter(categoria => categoria.id === id);
+        return categoriaArray.length === 0 ? null : categoriaArray[0];
+    }
+
+    static getProductosPorCategorias(categorias) {
+        return productos.filter(producto => categorias.includes(producto.categoria));
+    }
+
+}
