e167179423a9b1c20e532f4b0a4262278e749e63
PabloMM03 <1V3rsus1@gmail.com>
2023-02-18 19:37:51 +0100

    cosas

--- DWES_PMM/2ºTrimestre/Unidad5/laravelProject/routes/web.php
+++ DWES_PMM/2ºTrimestre/Unidad5/laravelProject/routes/web.php
@@ -14,7 +14,9 @@ use App\Http\Controllers\HomeController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//  Route::get('/', function () {
+//       return view('welcome');
+//  });
 Route::get('/', HomeController::class)->name('home');
 
 // Route::get('cursos', [CursoController::class, 'index']);
