3989f5afb01d2edc0097b2a298f32687d3a074ae
PabloMM03 <1V3rsus1@gmail.com>
2023-02-23 09:32:38 +0100

    fuck

--- DWEC_PMM/2ºTrimestre/Citas-clientes/js/nuevocliente.js
+++ DWEC_PMM/2ºTrimestre/Citas-clientes/js/nuevocliente.js
@@ -1,1 +1,29 @@
 import {ControladorPHP as Controlador} from "./controlador.js";
+
+
+function crearListeners(){
+
+    document.getElementById('nombre');
+    document.getElementById('formulario').addEventListener('submit',validarForm, false);
+
+
+}
+
+function validarForm(e){
+     e.preventDefault();
+    if (!validarNombre()) {
+        return false;
+    }
+    formulario.submit();
+      
+}
+
+function validarNombre() {
+    const nombre = nombreInput.value;
+    if (nombre.length < 4 || nombre.length > 30) {
+      alert('El nombre debe tener entre 4 y 30 caracteres');
+      return false;
+    }
+    return true;
+  }
+  
