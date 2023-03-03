7e57ccfdc06e3711ca4783e3849bfea0714c6f4c
Pablo <1V3rsus1@gmail.com>
2023-03-03 22:06:03 +0100

    SISIIS

--- DWES_PMM/Pruebas/marketplace_proyectS2/resources/views/admin/index.blade.php
+++ DWES_PMM/Pruebas/marketplace_proyectS2/resources/views/admin/index.blade.php
@@ -8,6 +8,7 @@
 
 @section('content')
     <p>Bienvenido al panel de Adminitracion de tu tienda.</p>
+    <img width="70%" src="https://imgs.search.brave.com/YzAN7_AqfNutgEzzmsJpRlq0VZNX27NbCicKrGcj7Vk/rs:fit:711:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5Y/VmhPVHhJR3lKMjAz/WW1YRWlqeHh3SGFF/OCZwaWQ9QXBp" alt="">
 @stop
 
 @section('css')
