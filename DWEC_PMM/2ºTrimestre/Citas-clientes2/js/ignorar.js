c0f95db5cfe88c3a29ab826f27a94506a6b89de2
Pablo <1V3rsus1@gmail.com>
2023-03-03 17:18:28 +0100

    Cambios

--- DWEC_PMM/2ºTrimestre/Citas-clientes2/js/ignorar.js
+++
@@ -1,8 +0,0 @@
-import {ControladorPHP as Controlador} from "./controlador.js";
-
-document.getElementById("eliminarBD").addEventListener("click", eliminarDatosSesionEvento, false);
-
-async function eliminarDatosSesionEvento(e) {
-    e.preventDefault();
-    const respuesta = await Controlador.eliminarBD();
-}
