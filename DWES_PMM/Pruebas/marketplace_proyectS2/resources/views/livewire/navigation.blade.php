c94a8667e559fda26f02efa7771939d9fec18c8d
Pablo <1V3rsus1@gmail.com>
2023-03-07 19:46:43 +0100

    Proyecto Laravel Tienda Online

--- DWES_PMM/Pruebas/marketplace_proyectS2/resources/views/livewire/navigation.blade.php
+++ DWES_PMM/Pruebas/marketplace_proyectS2/resources/views/livewire/navigation.blade.php
@@ -40,7 +40,7 @@
   
               <a href="/" style="text-decoration:none" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Tienda</a>
   
-              <a href="#" style="text-decoration:none" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Projects</a> 
+              {{-- <a href="#" style="text-decoration:none" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Projects</a>  --}}
                 
                 {{-- @foreach ($productos as $producto)
                         <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">{{$producto->name}}</a>
@@ -75,8 +75,8 @@
             <div x-show="open" x-on:click.away="open = false" class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
               <!-- Active: "bg-gray-100", Not Active: "" -->
               {{-- {{route('profile.show')}}--}}
-              <a href="#" style="text-decoration:none" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Perfil</a>
-              <a href="#" style="text-decoration:none" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">Ajustes</a>
+              {{-- <a href="#" style="text-decoration:none" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Perfil</a> --}}
+              {{-- <a href="#" style="text-decoration:none" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">Ajustes</a> --}}
 
 
               @can('admin.home')
@@ -116,7 +116,7 @@
   
         <a href="#" style="text-decoration: none" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Team</a>
   
-        <a href="#" style="text-decoration: none" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Projects</a>
+        {{-- <a href="#" style="text-decoration: none" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Projects</a> --}}
   
         <a href="#" style="text-decoration: none" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Calendar</a>
       </div>
